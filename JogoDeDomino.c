ca164321ea0ea0c306cc66ad97aa173d6850dbca
Orlando Mota Pires <orlandomota2412@hotmail.com>
2021-10-21 23:39:05 -0300

    Ultimas alterações

--- JogoDeDomino.c
+++ JogoDeDomino.c
@@ -7,7 +7,6 @@
  Leonardo Trinchão, Github: github.com/leotrinchao
  */
 
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -16,6 +15,13 @@
 #include <time.h>
 #include "pilha.h"
 #include "fila.h"	
+#include"LIstaSEncadeada.h"
+#include <Windows.h>
+
+	typedef struct{
+		int esquerda, direita; 
+	}tp_pedra;  
+
 
 void EscolhePrimeiro(int *escolhido, char *j1, char *j2, char *j3, char *j4){
 	switch (*escolhido){
@@ -35,38 +41,39 @@ void EscolhePrimeiro(int *escolhido, char *j1, char *j2, char *j3, char *j4){
 }
 
 void Sorteador(int *possibilidade, int *escolhido){ //Esse funcao vai sortear com base no inteiro que recebeu.
-	int random, nrandom;
-	long double	chance;
+	int random, nrandom, choosen;
+	float chance;
 	
-	srand(time (NULL)); //Generate seed for rand(),based in your pc time.
-	//printf("%d\n", *possibilidade);
-	random = (rand() % 100); //Generate random number in range 1-100.
-	//printf("random: %d\n", random);
-	chance =  100 / *possibilidade; //chance of getting any number.
-	//printf("chance: %lf \n", chance);
+	//srand(time (NULL)); //Generate seed for rand(),based in your pc time.
+	printf("Possibilidade: %d\n", *possibilidade);
+	random = rand() % 100 ; //Generate random number in range 1-100.
+	printf("random: %d\n", random);
+	chance = 100 / *possibilidade; //chance of getting any number.
+	printf("chance: %f \n", chance);
 
 	nrandom = 0;
 	*escolhido = 0;
 	
 	while(nrandom < random){ //Look for the range.
 		nrandom = nrandom  + chance;
-	//	printf("escolhido: %d\n", *escolhido);
-	//	printf("nrandom: %d\n", nrandom);
+		//printf("escolhido: %d\n", *escolhido);
+		//printf("nrandom: %d\n", random);
 		*escolhido = *escolhido + 1;	
 	}
-	//printf("escolhido: %d\n", *escolhido);
+	printf("escolhido: %d\n", *escolhido);
 }
-void MenuDoJogo(char *res){ //Função reponsável pelo menu inicial do jogo.
+
+void MenuDoJogo(int *res){ //Função reponsável pelo menu inicial do jogo.
  
 	printf("Bem-vindo ao MELHOR jogo de dominó da AMERA-LATINA e afiliados.\n");
 	printf("Insira o número 1 para jogar ou 0 para arregar :'(\n");
-	scanf(" %[^\n]s", res);
+	scanf(" %d", res);
 	//printf(" |%s| ", res);
 
-	while(*res != '0' & *res != '1' ){
+	while((*res != 0) && (*res != 1) ){
 		printf("Valor inválido.\n");
     	printf("Insira um valor válido, 1 para jogar ou 0 para arregar:\n");
-		scanf(" %[^\n]s", res);;
+		scanf(" %d", res);;
     //	printf(" |%s|\n", res);
 	}
 	system("pause");
@@ -124,35 +131,111 @@ void EscolheOsNomesDosJogadores(int *numj, char *j1, char *j2, char *j3, char *j
 	system("cls"); 
 }
 
-typedef struct{
-  int top, bottom; //EITA BIXO SEXOO
-}tp_pedra;
 
-tp_pedra pedra00, pedra01, pedra02, pedra03, pedra04, pedra05, pedra06;
-tp_pedra pedra11, pedra21, pedra31, pedra41, pedra51, pedra61, pedra22;
-tp_pedra pedra32, pedra42, pedra52, pedra62, pedra33, pedra43, pedra53; 
-tp_pedra pedra63, pedra44, pedra54, pedra64, pedra55, pedra65, pedra66; // Declarando todas as pecas do jogo ** NAO SEI SE VAI SER ASSIM **
+// tp_pedra pedra00, pedra01, pedra02, pedra03, pedra04, pedra05, pedra06;
+// tp_pedra pedra11, pedra21, pedra31, pedra41, pedra51, pedra61, pedra22;
+// tp_pedra pedra32, pedra42, pedra52, pedra62, pedra33, pedra43, pedra53; 
+// tp_pedra pedra63, pedra44, pedra54, pedra64, pedra55, pedra65, pedra66;
 
-
-void embaralhandoPecas(){
+void startamonte(tp_pedra *monte){ //Funcao que vai definir o valor de cada ladeo da peca dentor do vetor.
+	int contGeral, cont, numerodapeca;
+	contGeral = 0;
+	cont = 0;
+	numerodapeca = 6;
+	while(numerodapeca > -1){	// Preenche a parte esquerda das pecas
+ 		while(cont < (numerodapeca + 1)){
+ 			monte[contGeral].esquerda = numerodapeca;
+			//printf("%d ", monte[contGeral].esquerda);
+ 			contGeral ++;
+ 			cont ++;
+ 		}
+ 	cont = 0;
+ 	numerodapeca--;
+	}
+	//printf("\n");
+	contGeral = 0;
+	cont = 7;
+	numerodapeca = 0;
+	
+	while(cont > 0){ // Preenche a parte direita das pecas.
+		while(numerodapeca < cont){
+ 			monte[contGeral].direita = numerodapeca;
+			//printf("%d ", monte[contGeral].direita);
+			numerodapeca++;
+			contGeral++;
+		}
+	cont--;
+	numerodapeca = 0;
+	}
 
 }
+
+void sorteiaomonte(tp_pedra *monte){ //Funcao que vai embaralhar as pecas dentro de um pilha.
+	int peca, peca1,cont,num;
+	tp_pedra aux;
+	cont = 0;
+	num = 28;
+	
+		// while(cont< 500){	
+		
+	  // 	Sorteador(&num, &peca);
+	  // 	//printf("%d ", peca);
+	  // 	Sorteador(&num, &peca1);
+	  // 	//printf("%d ", peca1);
+	  // 	//printf("\n");
+	  // 	aux = monte[peca];
+	 	// monte[peca] = monte[peca1];
+	 	// monte[peca1] = aux;
+	  // 	cont++;
+	  // }
+		srand(time(NULL));
+		for(int i = 0; i < 500; i++){
+			peca = rand()%28;
+			peca1 = rand()%28;
+			aux = monte[peca];
+	 		monte[peca] = monte[peca1];
+	 	 	monte[peca1] = aux;
+		}
+		
+	cont = 0;
+	 while(cont < 28){
+	 	printf("%d ", monte[cont].esquerda);
+	 	cont++;
+	 }
+	 printf("\n");
+	 cont = 0;
+	 	while(cont < 28){
+	 	printf("%d ", monte[cont].direita);
+	 	cont++;
+	 }
+}
+
+
+
+
 int main(){
 	//Declarando as váriaveis.
 	setlocale(LC_ALL, "Portuguese"); //Definindo a liguagem para português.
-	int numj, choosen;
-	char res[100], j1[100], j2[100],j3[100],j4[100];
+	int numj, choosen, res;
+	char j1[100], j2[100],j3[100],j4[100];
+	tp_pedra monte[28];
+
 	
+	startamonte(monte);	
+	sorteiaomonte(monte);
+	system("pause");
 	//Começando o jogo.
-	MenuDoJogo(res); //Iniciando o menu jogo.
+	MenuDoJogo(&res); //Iniciando o menu jogo.
 	
-	if (res == '0'){  //Não vai jogar/Deistencia.
+	if (res == 0){  //Não vai jogar/Deistencia.
 		printf("Percebi que voce desistiu. FRACO!\n"); 
 	}
 	else{      //Vai jogar.
 		EscolheOsNomesDosJogadores(&numj, j1, j2, j3, j4);
 		Sorteador(&numj, &choosen); //sorteia quem comeca.
-		EscolhePrimeiro(&choosen, j1, j2, j3, j4);	
+		EscolhePrimeiro(&choosen, j1, j2, j3, j4);
+		startamonte(monte);	
+	
 	}
-	return 0;
-} 
+	return 0;              // :(
+}
---
+++ LIstaSEncadeada.h
@@ -0,0 +1,141 @@
+#ifndef LISTASE_H
+#define LISTASE_H
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int tp_item;
+
+typedef struct tp_no{
+  tp_item info;
+  struct tp_no *prox;
+}tp_listase;
+
+tp_listase *inicializa_listase(){
+  return NULL;
+}
+
+int listase_vazia(tp_listase *lista){
+  if(lista==NULL) return 1;
+  else return 0;
+}
+
+tp_listase *aloca_listase(){
+  tp_listase *novo_no;
+  novo_no = (tp_listase*) malloc(sizeof(tp_listase));
+  return novo_no;
+}
+
+int insere_listase_no_fim(tp_listase **l, tp_item e){
+  tp_listase *novo_no, *atu;
+  novo_no = aloca_listase();
+  if(novo_no == NULL) return 0;
+  novo_no->info = e;
+  novo_no->prox = NULL;
+
+  if(listase_vazia(*l)){
+    *l = novo_no;
+  } else{
+    atu = *l;
+    while(atu->prox != NULL){
+      atu = atu->prox;
+    }
+    atu->prox=novo_no;
+  }
+
+return 1;
+}
+
+void imprime_listase(tp_listase *lista){
+  tp_listase *atu;
+  atu = lista;
+  while(atu!=NULL){
+    printf("%d\n", atu->info);
+    atu=atu->prox;
+  }
+}
+
+int remove_listase(tp_listase **lista, tp_item e){
+  tp_listase *ant, *atu;
+  atu = *lista;
+  ant = NULL;
+  while((atu!=NULL) && (atu->info!=e)){
+    ant = atu;
+    atu = atu->prox; }
+  if(atu == NULL) return 0;
+  if(ant == NULL){
+    *lista = atu->prox;
+  }else{
+    ant->prox = atu->prox;    
+  }
+  free(atu);
+  atu = NULL;
+  return 1;
+}
+
+tp_listase *busca_listase(tp_listase *lista, tp_item e){
+  tp_listase *atu;
+  atu = lista;
+  while((atu!=NULL) && (atu->info!=e)){
+    atu = atu->prox; }
+  if(atu == NULL) return NULL;
+  return atu;
+}
+
+int tamanho_listase(tp_listase *lista){
+  int cont = 0;
+  tp_listase *atu;
+  atu=lista;
+  while(atu!=NULL){
+    cont++;
+    atu=atu->prox;
+  }
+  return cont;
+}
+
+int insere_listase_no_inicio(tp_listase **l, tp_item e){
+  tp_listase *novo_no;
+  novo_no = aloca_listase();
+  if(novo_no == NULL) return 0;
+  novo_no->info = e;
+  if(listase_vazia(*l)){
+    novo_no->prox = NULL;
+  } else novo_no->prox = *l;
+  *l = novo_no;
+  
+return 1;
+}
+
+int contagem_maior_que_n(tp_listase *l, tp_item e){
+  int cont = 0;
+  tp_listase *atu;
+  atu=l;
+  while(atu!=NULL){
+    if(atu->info > e) cont++;
+    atu=atu->prox;
+  }
+  return cont;
+}
+
+void desfaz_lista(tp_listase **lista){
+  tp_listase *atu;
+  atu = *lista;
+  while(atu!=NULL){
+    *lista = atu->prox;
+    free(atu);
+  }
+}
+
+int checagem_igual(tp_listase *lista1, tp_listase *lista2){
+  tp_listase *atu1, *atu2;
+  atu1 = lista1;
+  atu2 = lista2;
+  if(tamanho_listase(lista1) != tamanho_listase(lista2) ) return 0;
+  while(atu1!=NULL){
+    if(atu1->info != atu2->info) return 0;
+    atu1=atu1->prox;
+    atu2=atu2->prox;    
+  }
+return 1;
+}
+
+#endif
